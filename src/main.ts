74383f02bf400acffae355dc6112887cb2c16008
romaintailhurat <romain.tailhurat@gmail.com>
2021-08-20 10:09:51 +0000

    First try at scenes logic

--- src/main.ts
+++ src/main.ts
@@ -15,23 +15,30 @@ import {
 } from "excalibur";
 import { Resources } from "./resources";
 import { Planet, PlanetArgs } from "./planet";
+import { SystemViewScene } from "./scenes/SystemViewScene";
+import { OrbitViewScene } from "./scenes/OrbitViewScene";
 
 class Game extends Engine {
   constructor() {
     super({ width: 800, height: 600 });
   }
+
   initialize() {
+    
+
     // ------ UI
-    const planetsInfo = new Label({text: "info", pos: new Vector(100,10)});
-    planetsInfo.enableCapturePointer = true;
-    planetsInfo.on("pointerenter", e => {console.log("desperate")});
+    const planetsInfo = new Label({ text: "info", pos: new Vector(100, 10) });
     this.add(planetsInfo);
 
     // ----- Planets
+    const systemView = new SystemViewScene();
+
     const rand = new Random();
     const numberOfPlanets = rand.integer(1, 5);
 
     planetsInfo.text = `This system has ${numberOfPlanets} planets`;
+
+    systemView.add(planetsInfo);
 
     // Random system =)
     for (let i = 1; i < numberOfPlanets + 1; i++) {
@@ -51,11 +58,27 @@ class Game extends Engine {
       };
 
       const p = new Planet(pArgs);
-      this.add(p);
+      systemView.add(p);
     }
 
+    
+
+    const orbitView = new OrbitViewScene();
+
+    this.addScene(systemView.key, systemView);
+    this.addScene(orbitView.key, orbitView);
+
+    // ----- Subscribing to events
+    this.on("toorbitview", (e) => {
+      console.log(e);
+      this.goToScene(orbitView.key);
+    })
+
+    // ----- Starting
     const loader = new Loader([Resources.Sword]);
-    this.start(loader);
+    this.start(loader).then(() => {
+      this.goToScene(systemView.key);
+    });
   }
 }
 
--- src/planet.ts
+++ src/planet.ts
@@ -1,21 +1,44 @@
-import { Actor, ActorArgs, Graphics, Random } from "excalibur";
+import { Actor, ActorArgs, GameEvent, Graphics, Random, Shape } from "excalibur";
 
 const randomName = (): string => {
   const rand = new Random();
-  const cons = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"];
-  const vows = ["a","e","i","o","u","y"];
-  const len = rand.integer(4,10);
+  const cons = [
+    "b",
+    "c",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "v",
+    "w",
+    "x",
+    "z",
+  ];
+  const vows = ["a", "e", "i", "o", "u", "y"];
+  const len = rand.integer(4, 10);
   let name = "";
   for (let i = 0; i < len; i++) {
     const isVowel = rand.bool(0.4);
-    if(isVowel) {
+    if (isVowel) {
       name = name + rand.pickOne(vows);
     } else {
       name = name + rand.pickOne(cons);
     }
   }
-  return(name);
-}
+  return name;
+};
+
+export class ToOrbitViewEvent extends GameEvent<Planet>{}
 
 export interface PlanetArgs extends ActorArgs {
   radius: number;
@@ -28,26 +51,31 @@ export class Planet extends Actor {
   constructor(opts: PlanetArgs) {
     super(opts);
     this.name = randomName();
+    this.body.collider.shape = Shape.Circle(this.radius);
+    console.log(this.body);
   }
 
   toString() {
-    return(`Planet ${this.name}`);
+    return `Planet ${this.name}`;
   }
 
   onInitialize() {
-    this.width = this.radius * 2;
-    this.height = this.radius * 2;
-
     const planetShape = new Graphics.Circle({
       radius: this.radius,
       color: this.color,
-    });    
+    });
 
     this.graphics.add(planetShape);
-    
+
     this.enableCapturePointer = true;
-    this.on("pointerup", () => {      
-      console.log(this.toString());
+
+    this.on("pointerenter", () => {
+      //console.log(this.toString());
+    });
+
+    this.on("pointerup", () => {
+      console.log("emitting GO_TO_ORBIT_VIEW");
+      this.emit("toorbitview", new ToOrbitViewEvent());
     });
   }
 }
