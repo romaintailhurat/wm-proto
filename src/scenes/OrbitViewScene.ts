b532bbe2789f96b70c1bbe0c790fedb7ddb24d3e
romaintailhurat <romain.tailhurat@gmail.com>
2021-08-24 17:26:33 +0200

    :computer: Configuring  for orbit view

--- src/scenes/OrbitViewScene.ts
+++ src/scenes/OrbitViewScene.ts
@@ -1,10 +1,11 @@
-import { Scene } from "excalibur";
+import { Scene, vec } from "excalibur";
 import { PlanetActor } from "../actors/PlanetActor";
 import { StateManager } from "../state/StateManager";
 
 export class OrbitViewScene extends Scene {
   // Used when adding the scene to the game
   public key: string;
+  public scaleFactor: number = 3;
 
   static getViewKey = () => "orbitviewscene";
 
@@ -19,7 +20,8 @@ export class OrbitViewScene extends Scene {
     console.log(`→ ${this.key} - Planet is : ${planet.name}`);
     const planetActor = new PlanetActor({
       planet: planet,
-      radius: planet.radius,
+      radius: planet.radius * this.scaleFactor,
+      pos: vec(this.engine.canvasWidth * 2 / 3, this.engine.canvasHeight * 0.5),
     });
 
     this.add(planetActor);
